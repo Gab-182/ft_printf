57e0ab5c44c7a95d687a462abc9aad6d236a8fe6
Ghaith Abdoush <ghaith181994@hotmail.com>
2021-11-26 11:49:28 +0400

    merging ft_itoh.c to the utils file

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -4,7 +4,7 @@ void     ft_putchar(int c)
 {
      write (1, &c, 1);
 }
-
+/******************************************************/
 void    ft_putstr(char *s)
 {
     while (*s)
@@ -13,7 +13,7 @@ void    ft_putstr(char *s)
         s++;
     }
 }
-
+/******************************************************/
 void	ft_putnbr(int n)
 {
 	if (n == -2147483648)
@@ -36,7 +36,7 @@ void	ft_putnbr(int n)
 		ft_putchar((char)(n + '0'));
 	}
 }
-
+/******************************************************/
 void	ft_putnbr_unsigned(unsigned int n)
 {
     if (n >= 10)
@@ -49,97 +49,49 @@ void	ft_putnbr_unsigned(unsigned int n)
 		ft_putchar((char)(n + '0'));
 	}
 }
-
-// void	ft_bzero(void *dest, size_t len)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (i < len)
-// 	{
-// 		((unsigned char *)dest)[i] = 0;
-// 		i++;
-// 	}
-// }
-
-// size_t	ft_intlen(int num)
-// {
-// 	size_t	i;
-
-// 	i = 1;
-// 	num = num / 10;
-// 	while (num)
-// 	{
-// 		i++;
-// 		num = num / 10;
-// 	}
-// 	return (i);
-// }
-
-// char	*ft_itoa(int n)
-// {
-// 	char			*str;
-// 	size_t			intlen;
-// 	unsigned int	num_copy;
-
-// 	intlen = ft_intlen(n);
-// 	num_copy = n;
-// 	if (n < 0)
-// 	{
-// 		num_copy = -1 * n;
-// 		intlen++;
-// 	}
-// 	str = (char *)malloc(sizeof(char) * intlen + 1);
-// 	ft_bzero(str, intlen + 1);
-// 	if (!str)
-// 		return (NULL);
-// 	str[--intlen] = num_copy % 10 + '0';
-// 	num_copy = num_copy / 10;
-// 	while (num_copy)
-// 	{
-// 		str[--intlen] = num_copy % 10 + '0';
-// 		num_copy = num_copy / 10;
-// 	}
-// 	if (n < 0)
-// 		*(str + 0) = '-';
-// 	return (str);
-// }
-
-void	ft_itoh(int n)
+/******************************************************/
+void ft_itoh(int num)
 {
-	if (n == -2147483648)
-	{
-		ft_putstr("-80000000");
-	}
-	else if (n < 0)
-	{
-		ft_putchar('-');
-		n = -1 * n;
-		ft_putnbr(n);
-	}
-	else if (n > 9 && n < 16)
-	{
-		if (n == 10)
-			ft_putchar('a');
-		if (n == 11)
-			ft_putchar('b');
-		if (n == 12)
-			ft_putchar('c');
-		if (n == 13)
-			ft_putchar('d');
-		if (n == 14)
-			ft_putchar('e');
-		if (n == 15)
-			ft_putchar('f');
-	}
-	else if (n > 16)
-	{
-		ft_putnbr(n / 10);
-		ft_putchar(n % 10 + '0');
-	}
-	else if (n < 10)
-	{
-		ft_putchar((char)(n + '0'));
-	}
+    int dev;
+    int rem;
+    int rem_mult_16;
+    char hex;
+    
+    while (dev > 0)
+    {
+        dev = num/16;
+        rem = num % 16;
+        printf("dev is : %d\n", dev);
+        printf("rem is : %d\n", rem);
+        num = dev;
+        rem_mult_16 = rem * 16;
+        if (rem_mult_16 > 9)
+        {
+            if (rem_mult_16 == 10 )
+            {
+                hex = 'a';
+            }
+            else if (rem_mult_16 == 11 )
+            {
+                hex = 'b';
+            }
+            else if (rem_mult_16 == 12 )
+            {
+                hex = 'c';
+            }
+            else if (rem_mult_16 == 13 )
+            {
+                hex = 'd';
+            }
+            else if (rem_mult_16 == 14 )
+            {
+                hex = 'e';
+            }
+            else if (rem_mult_16 == 15 )
+            {
+                hex = 'f';
+            }
+        }
 
+    }
 }
--- main.c
+++ main.c
@@ -2,42 +2,44 @@
 
 int main()
 {
-	int d = 123;
-	int i = 123456;
-	int x = -2147483648;
-	/**
-	 * The max value for unsigned int is : 4,294,967,295
-	 * if you put 4,294,967,296 
-	 * there will be a overflow in the value of unsigned int and
-	 * it will print (0).
-	*/
-	unsigned int u = 4294967295;
-	char c = 'W';
-	char *str = "  Ghaiath Abdoush  ";
+// 	int d = 123;
+// 	int i = 123456;
+// 	int x = -2147483648;
+// 	/**
+// 	 * The max value for unsigned int is : 4,294,967,295
+// 	 * if you put 4,294,967,296 
+// 	 * there will be a overflow in the value of unsigned int and
+// 	 * it will print (0).
+// 	*/
+// 	unsigned int u = 4294967295;
+// 	char c = 'W';
+// 	char *str = "  Ghaiath Abdoush  ";
 
-	ft_printf("%d ", d);
-	printf("\n");
-/********************************/
-	ft_printf("%c", c);
-	printf("\n");
-/********************************/
-	ft_printf("%i", 123456);
-	printf("\n");
-/********************************/
-	ft_printf("%s", str);
-	printf("\n");
-/********************************/
-	ft_printf("%u", u);
-	printf("\n");
-/********************************/
-    ft_printf("%%");
-	printf("\n");
-/********************************/
-	ft_printf("%x", x);
-	printf("\n");
-/********************************/
-	ft_printf("%d %c %s %i %u %x", d, c, str, i, u, x);
-	printf("\n");
+// 	ft_printf("%d ", d);
+// 	printf("\n");
+// /********************************/
+// 	ft_printf("%c", c);
+// 	printf("\n");
+// /********************************/
+// 	ft_printf("%i", 123456);
+// 	printf("\n");
+// /********************************/
+// 	ft_printf("%s", str);
+// 	printf("\n");
+// /********************************/
+// 	ft_printf("%u", u);
+// 	printf("\n");
+// /********************************/
+//     ft_printf("%%");
+// 	printf("\n");
+// /********************************/
+// 	ft_printf("%x", x);
+// 	printf("\n");
+// /********************************/
+// 	ft_printf("%d %c %s %i %u %x", d, c, str, i, u, x);
+// 	printf("\n");
+
+	ft_itoh(1128);
     
 	return (0);
 }
