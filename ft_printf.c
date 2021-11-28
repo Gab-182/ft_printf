7c57da154596091c5f0b9d01c614d3d8947405ce
Ghaith Abdoush <ghaith181994@hotmail.com>
2021-11-28 20:15:25 +0400

    finishing the pointer part for ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gabdoush <gabdoush@42ABUDHABI.AE>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 11:04:22 by gabdoush          #+#    #+#             */
-/*   Updated: 2021/11/28 11:48:19 by gabdoush         ###   ########.fr       */
+/*   Updated: 2021/11/28 20:13:00 by gabdoush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,69 +14,98 @@
 
 void ft_printf(const char* para, ...)
 {
+	/**
+	 * para ---> is a character pointer that will point to all
+	 * 			 the arguments that will be passed to the ft_printf()
+	 * 			 function.
+	*/
+
+	/* va_list ----> using this to define the list of arguments */
     va_list  args;
+	/* va-start ----> to start the argument list. */
 	va_start (args, para);
 	int i;
 
-/* Looping in the flags*/
+/* Looping in the arguments*/
 	i = 0;
+	
 	while (para[i] != '\0')
 	{
-		if (para[i] == 'd')
+	/********************************/	
+		if (para[i] == ' ')
 		{
-			int d;
-			d = va_arg(args, int);
-			ft_putnbr(d);
+			ft_putchar(' ');
 		}
-		else if (para[i] == 'i')
+		if (para[i] == '%')
 		{
-			int i;
-			i = va_arg(args, int);
-			ft_putnbr(i);
+			i++;
+			int j = i;
+			while (para[j] != '0')
+			{
+				if (para[j] == '%')
+					ft_putchar('%');
+					break;
+				j++;
+			}
+			if (para[i] == ' ')
+			{
+				i++;
+			}
+			else if (para[i] == 'd')
+			{
+				int d;
+				d = va_arg(args, int);
+				ft_putnbr(d);
+			}
+			else if (para[i] == 'i')
+			{
+				int i;
+				i = va_arg(args, int);
+				ft_putnbr(i);
+			}
+			else if (para[i] == 'u')
+			{
+				unsigned int u;
+				u = va_arg(args, unsigned int);
+				ft_putnbr_unsigned(u);
+			}
+			else if (para[i] == 'c')
+			{
+				int c;
+				c = va_arg(args, int);
+				ft_putchar(c);
+			}
+			else if (para[i] == 's')
+			{
+				char *s;
+				s = va_arg(args, char*);
+				ft_putstr(s);
+			}
+			else if (para[i] == 'x')
+			{
+				int num;
+				num = va_arg(args, int);
+				ft_putnbr_x(num);
+			}
+			else if (para[i] == 'X')
+			{
+				int num;
+				num = va_arg(args, int);
+				ft_putnbr_X(num);
+			}
+			else if (para[i] == 'p')
+			{
+				unsigned long long int pointer;
+				pointer = va_arg(args, unsigned long long int);
+				ft_putstr("0x");
+				ft_putnbr_p(pointer);
+			}
 		}
-		else if (para[i] == '%' && para[i+1] == '%')
+		else
 		{
-			int percent;
-			percent = va_arg(args,int);
-			ft_putchar('%');
+			ft_putchar(para[i]);
 		}
-		else if (para[i] == 'u')
-		{
-			unsigned int u;
-			u = va_arg(args, unsigned int);
-			ft_putnbr_unsigned(u);
-		}
-		else if (para[i] == 'c')
-		{
-			int c;
-			c = va_arg(args, int);
-			ft_putchar(c);
-		}
-		else if (para[i] == 's')
-		{
-			char *s;
-			s = va_arg(args, char*);
-			ft_putstr(s);
-		}
-		else if (para[i] == 'x')
-		{
-			int num;
-			num = va_arg(args, int);
-			ft_putnbr_x(num);
-		}
-		else if (para[i] == 'X')
-		{
-			int num;
-			num = va_arg(args, int);
-			ft_putnbr_X(num);
-		}
-		else if (para[i] == 'p')
-		{
-			unsigned long long int pointer;
-			pointer = va_arg(args, unsigned long long int);
-			ft_putstr("0x");
-			ft_putnbr_p(pointer);
-		}
+	/********************************/
 		i++;
 	}
 	va_end(args);
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-	int d = 123;
+ 	int d = 123;
 	int i = 123456;
 	int x = 12345678;
 	int X = 12345678;
@@ -19,36 +19,37 @@ int main()
 	char c = 'W';
 	char *str = "  Ghaiath Abdoush  ";
 
-/*********** Decimal ************/
-	ft_printf("%d ", d);
+// /*********** Decimal ************/
+// 	ft_printf("%d ", d);
+// 	printf("\n");
+// /************* char *************/
+// 	ft_printf("%c", c);
+// 	printf("\n");
+// /*********** Integer ************/
+// 	ft_printf("%i", 123456);
+// 	printf("\n");
+// /*********** String *************/
+// 	ft_printf("%s", str);
+// 	printf("\n");
+// /****** unsigned integer ********/
+// 	ft_printf("%u", u);
+// 	printf("\n");
+// /*********** percent ************/
+//     ft_printf("%%");
+// 	printf("\n");
+// /******** hexadecimal x *********/
+// 	ft_printf("%x", x);
+// 	printf("\n");
+// /******** hexadecimal X *********/
+// 	ft_printf("%X", X);
+// 	printf("\n");
+// /********* Pointer **************/
+// 	ft_printf("%p", p);
+// 	printf("\n");
+// /********************************/
+	ft_printf("333%d333 %c123 %s %i123    %% %u    %x %X    %p", d, c, str, i, u, x, X, p);
 	printf("\n");
-/************* char *************/
-	ft_printf("%c", c);
-	printf("\n");
-/*********** Integer ************/
-	ft_printf("%i", 123456);
-	printf("\n");
-/*********** String *************/
-	ft_printf("%s", str);
-	printf("\n");
-/****** unsigned integer ********/
-	ft_printf("%u", u);
-	printf("\n");
-/*********** percent ************/
-    ft_printf("%%");
-	printf("\n");
-/******** hexadecimal x *********/
-	ft_printf("%x", x);
-	printf("\n");
-/******** hexadecimal X *********/
-	ft_printf("%X", X);
-	printf("\n");
-/********* Pointer **************/
-	ft_printf("%p", p);
-	printf("\n");
-/********************************/
-	// ft_printf("%d %c %s %i %u %x %p", d, c, str, i, u, x, p);
-	// printf("\n");
+	printf("333%d333 %c123 %s %i123    %% %u    %x %X    %p", d, c, str, i, u, x, X, p);
 
 	return (0);
 }
