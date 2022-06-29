1f6c00c710983672186856cdf4f91047c6a6ff6f
Ghaith Abdoush <83855149+Gab-182@users.noreply.github.com>
2022-06-29 17:04:39 +0400

    Update integer_argument.c
    Fixing the header issue

--- To_Submit/integer_argument.c
+++ To_Submit/integer_argument.c
@@ -5,13 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gabdoush <gabdoush@42ABUDHABI.AE>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2021/12/13 09:56:25 by gabdoush          #+#    #+#             */
 /*   Updated: 2021/12/15 01:44:05 by gabdoush         ###   ########.fr       */
-=======
 /*   Created: 2021/12/23 03:47:56 by gabdoush          #+#    #+#             */
 /*   Updated: 2021/12/23 03:51:02 by gabdoush         ###   ########.fr       */
->>>>>>> 1e22491c2c7da4f01d77153a4d57e9a3fdd94bb8
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +24,3 @@ int	integer_argument(va_list args)
 	len = ft_putnbr(d);
 	return (len);
 }
-<<<<<<< HEAD
- 
-=======
->>>>>>> 1e22491c2c7da4f01d77153a4d57e9a3fdd94bb8
